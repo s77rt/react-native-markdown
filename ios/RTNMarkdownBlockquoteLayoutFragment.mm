99d3984badd271121c430825b46620471c9d51a4
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-02-17 18:45:09 +0100

    iOS: Exclude empty text line fragments from stripe height calculation. Fixes #11

--- ios/RTNMarkdownBlockquoteLayoutFragment.mm
+++ ios/RTNMarkdownBlockquoteLayoutFragment.mm
@@ -15,10 +15,17 @@
 - (void)drawAtPoint:(CGPoint)point inContext:(CGContextRef)context {
   [super drawAtPoint:point inContext:context];
 
+  CGFloat stripeHeight = 0;
+  for (NSTextLineFragment *textLineFragment in self.textLineFragments) {
+    if (textLineFragment.characterRange.length == 0) {
+      continue;
+    }
+    stripeHeight += textLineFragment.typographicBounds.size.height;
+  }
+
   for (int i = 1; i <= self.indentationLevel; i++) {
-    CGRect rectangle =
-        CGRectMake(-(self.gapWidth + self.stripeWidth) * i, 0, self.stripeWidth,
-                   self.renderingSurfaceBounds.size.height);
+    CGRect rectangle = CGRectMake(-(self.gapWidth + self.stripeWidth) * i, 0,
+                                  self.stripeWidth, stripeHeight);
     CGContextSetFillColorWithColor(context, [self.stripeColor CGColor]);
     CGContextFillRect(context, rectangle);
   }
